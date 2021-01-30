84a46f5b767c58798f3a53a966d35d7e23d8bfa1
amsaabesh <mahiraabesh@gmail.com>
2021-01-30 13:43:07 +0600

    Fixed

--- fixed_possition_Iteration.cpp
+++ fixed_possition_Iteration.cpp
@@ -1,28 +1,22 @@
-#include<stdio.h>
-#include<math.h>
-#define f(x) x*x*x+x*x-1
-#define g(x) (1/sqrt(1+x0))
-int main()
-{
-	 int i=1,N;
-	 float x0,x1,e;
-	 printf("Initial guess?\n");
-	 scanf("%f", &x0);
-	 printf("error?\n");
-	 scanf("%f", &e);
-	 printf("Max Iter. Lim.?\n");
-	 scanf("%d", &N);
+#include<bits/stdc++.h>
+using namespace std;
+#define f(x) pow(x,3)+pow(x,2)-1
+#define g(x) (1/sqrt(1+init))
+int main(){
+	 int i,N;
+	 float init,x1,err;
+	 printf("Initial guess?,error?,Max Iter. Lim.?\n");
+	 scanf("%f%f%f",&init, &err, &N);
 	 printf("\nStep\tx0\t\tf(x0)\t\tx1\t\tf(x1)\n");
-	 for(i=1;fabs(f(x1))>e;i++)
+	 for(i=1;fabs(f(x1))>err;i++)
 	 {
-		  x1=g(x0);
-		  printf("%d\t%f\t%f\t%f\t%f\n",i,x0,f(x0),x1,f(x1));
-		  if(i>N)
-		  {
+		  x1=g(init);
+		  printf("%d\t%f\t%f\t%f\t%f\n",i,init,f(init),x1,f(x1));
+		  if(i>N){
 			   printf("Not Convergent.");
 		  }
-		  x0=x1;
+		  init=x1;
 	 }
-	 printf("\nRoot is %f", x1);
+	 printf("\nRoot = %f", x1);
 	 return(0);
 }
