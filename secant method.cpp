88f9b8e0adbf0411db8821a6fca852610cdeb50f
amsaabesh <mahiraabesh@gmail.com>
2021-01-30 12:53:12 +0600

    Secant method

--- secant method.cpp
+++ secant method.cpp
@@ -1,12 +1,10 @@
-#include <iostream>
-#include<math.h>
+#include<bits/stdc++.h>
 using namespace std;
 float func(float x){
     float f=pow(x,3)-x-1;
     return f;
 }
-void sec(float x1,float x2,float epsi)
-{
+void sec(float x1,float x2,float epsi){
     float n=0,xm,x0,c;
     if (func(x1)*func(x2) < 0) {
         while (fabs(x1-x2)>=epsi){
@@ -25,5 +23,4 @@ int main()
 {
     float x1=0,x2=2,epsi=0.00001;
     sec(x1,x2,epsi);
-    return 0;
 }
